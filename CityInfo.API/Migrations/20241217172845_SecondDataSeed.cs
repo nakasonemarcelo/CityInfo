db1339a2b90fa5f944abbe56211e19c696fcf6ac
nakasonemarcelo <nakasone.marcelo@outlook.com>
2024-12-20 18:32:08 -0300

    Termino do capitulo 8 Searching, Filtering and Paging Resources

---
+++ CityInfo.API/Migrations/20241217172845_SecondDataSeed.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CityId", "Description" },
+                values: new object[] { 2, "A Gothic style catedral, conceived by architects Jan and Pieter Appelmans." });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 4, 2, "The finest example of railway architecture in Belgium.", "Antwerp Central Station" },
+                    { 5, 3, "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel.", "Eiffel Tower" },
+                    { 6, 3, "The world's largest museum.", "The Louvre" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.UpdateData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CityId", "Description" },
+                values: new object[] { 1, "A Gothic Catedral" });
+        }
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,6 +1,21 @@
+using CityInfo.API;
+using CityInfo.API.DbContexts;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+//builder.Logging.ClearProviders();
+//builder.Logging.AddConsole();
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -15,6 +30,21 @@ AddXmlDataContractSerializerFormatters();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+
+#if DEBUG
+builder.Services.AddTransient<IMailService, LocalMailService>();
+#else
+builder.Services.AddTransient<IMailService, CloudMailService>();
+#endif
+builder.Services.AddSingleton<CitiesDataStore>();
+
+builder.Services.AddDbContext<CityInfoContext>(dbContextOptions 
+    => dbContextOptions.UseSqlite(
+        builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
+
+builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
